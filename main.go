b25d52c4f4bcff5154e61dafd3d5c19fe739bd45
Phil Adams <phil_adams@us.ibm.com>
2019-12-07 07:39:23 -0600

    Update lib dependency to v3.0.0

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	lib "github.com/padamstx/gomodlib/v2"
+	lib "github.com/padamstx/gomodlib/v3"
 )
 
-const version string = "1.0.1"
+const version string = "2.0.0"
 
 func main() {
 	fmt.Printf("nomodapp main, version %s\n", version)
