eab23f33df6b45ca07b90a89c2df87df49312899
Phil Adams <phil_adams@us.ibm.com>
2020-01-05 10:35:28 -0600

    Move to v3.0.2 of gomodlib, removed v3 from package name

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	lib "github.com/padamstx/gomodlib/v3"
+	lib "github.com/padamstx/gomodlib"
 )
 
-const version string = "2.0.0"
+const version string = "3.0.0"
 
 func main() {
 	fmt.Printf("nomodapp main, version %s\n", version)
